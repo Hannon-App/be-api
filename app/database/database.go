4f3c64f863f843ee19407917944d6829e3fb99a4
Jaya Rizky Prayoga <prayogarock666@gmail.com>
2023-09-26 21:09:06 +0700

    update skeleton

--- app/database/database.go
+++ app/database/database.go
@@ -1,1 +1,23 @@
 package database
+
+import (
+	"Hannon-app/app/config"
+	"fmt"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func InitMysql(cfg *config.AppConfig) *gorm.DB {
+	connectionString := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=True&loc=Local",
+		cfg.DBUsername, cfg.DBPassword, cfg.DBHost, cfg.DBPort, cfg.DBName)
+
+	DB, err := gorm.Open(mysql.Open(connectionString), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	return DB
+}
+
+func InittialMigration(db *gorm.DB) {
+}
--- app/middlewares/jwt-middlewares.go
+++ app/middlewares/jwt-middlewares.go
@@ -1,1 +1,37 @@
 package middlewares
+
+import (
+	"Hannon-app/app/config"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	echojwt "github.com/labstack/echo-jwt/v4"
+	"github.com/labstack/echo/v4"
+)
+
+func JWTMiddleware() echo.MiddlewareFunc {
+	return echojwt.WithConfig(echojwt.Config{
+		SigningKey:    []byte(config.JWT_SECRRET),
+		SigningMethod: "HS256",
+	})
+}
+
+func CreateToken(userId uint) (string, error) {
+	claims := jwt.MapClaims{}
+	claims["authorized"] = true
+	claims["userId"] = userId
+	claims["exp"] = time.Now().Add(time.Hour * 24).Unix()
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	return token.SignedString([]byte(config.JWT_SECRRET))
+}
+
+func ExtractTokenUserId(e echo.Context) uint {
+	user := e.Get("user").(*jwt.Token)
+	if user.Valid {
+		claims := user.Claims.(jwt.MapClaims)
+		userId := claims["userId"].(float64)
+		return uint(userId)
+	}
+	return 0
+}
