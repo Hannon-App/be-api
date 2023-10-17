5ce88021d02e24c373d23a611ebb857390df3baf
Firhan Ahmad F <firhanahmadf@gmail.com>
2023-10-17 10:49:45 +0700

    feat: Get VA payment by id

--- features/payments/data/query.go
+++ features/payments/data/query.go
@@ -12,6 +12,21 @@ type PaymentQuery struct {
 	db *gorm.DB
 }
 
+// GetVAById implements payments.PaymentDataInterface.
+func (repo *PaymentQuery) GetVAById(id string) (payments.VirtualAccountObjectCore, error) {
+	var data VirtualAccountObject
+	tx := repo.db.Where("id = ?", id).Find(&data)
+	if tx.Error != nil {
+		return payments.VirtualAccountObjectCore{}, tx.Error
+	}
+	if tx.RowsAffected == 0 {
+		return payments.VirtualAccountObjectCore{}, errors.New("data not found")
+	}
+
+	resultCore := VAModelToCore(data)
+	return resultCore, nil
+}
+
 // CreateVA implements payments.PaymentDataInterface.
 func (repo *PaymentQuery) CreateVA(input payments.VirtualAccountObjectCore) error {
 	var data = VACoreToModel(input)
